920019f4b0f05b861e890136e7eb57b231ca0ea1
estefa.932014 <estefa.932014@outlook.es>
2023-11-09 18:52:23 -0500

    Archivos de configuracion reto1

---
+++ ejercicio1/accessRepo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+LRED='\033[1;31m'
+LGREEN='\033[1;32m'
+NC='\033[0m'
+LBLUE='\033[0;34m'
+LYELLOW='\033[1;33m'
+repo="bootcamp-devops-2023"
+project="app-295devops-travel"
+branch="clase2-linux-bash"
+
+function execution_script_sql {
+    devopstravel_sql="devopstravel_sql="/var/www/html/database/devopstravel.sql""
+    mysql < $devopstravel_sql
+}
+
+if [ -d "$repo" ] ; then
+    echo "Existe repo"
+    cd $repo
+    git pull origin $branch
+    git checkout $branch
+    pwd
+else
+    echo "\n${LYELLOW}Se clonará el repositorio.${NC}"
+    git clone https://github.com/roxsross/$repo.git
+    cd $repo/
+    git checkout $branch
+fi
+function copyfiles {
+    cp -rf $project/* /var/www/html
+}
+#llamado a la función de copia de archivos estaticos
+copyfiles
+echo "\n${LBLUE}Archivos estaticos copiados.${NC}"
+#llamado a la función de copia de archivos estaticos
+execution_script_sql
+echo "\n${LBLUE}Script ejecutado.${NC}"
